2678a8bc66de5137780141a09e9e752c7fbb2276
Hikki0710 <buscarinoantonino@gmail.com>
2025-01-04 23:00:46 +0100

    feat(ramaorientalfansub): add the '/homepage' and '/watch' endpoints

--- websites/R/ramaorientalfansub/presence.ts
+++ websites/R/ramaorientalfansub/presence.ts
@@ -6,10 +6,79 @@ const enum Assets {
 	Logo = "https://i.imgur.com/V4hlmcx.png",
 }
 
+async function getStrings() {
+	return presence.getStrings(
+		{
+			paused: "general.paused",
+			play: "general.playing",
+			search: "general.searchFor",
+			viewCategory: "general.viewCategory",
+			viewHome: "general.viewHome",
+			viewShow: "general.viewShow",
+		},
+		await presence.getSetting<string>("lang").catch(() => "en")
+	);
+}
+
+let strings: Awaited<ReturnType<typeof getStrings>>,
+	oldLang: string = null,
+	current: number,
+	duration: number,
+	paused: boolean;
+
+presence.on(
+	"iFrameData",
+	(data: { current: number; duration: number; paused: boolean }) => {
+		({ current, duration, paused } = data);
+	}
+);
+
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: Assets.Logo,
-	};
+			largeImageKey: Assets.Logo,
+		},
+		[newLang, cover] = await Promise.all([
+			presence.getSetting<string>("lang").catch(() => "en"),
+			presence.getSetting<boolean>("cover"),
+		]),
+		{ pathname } = document.location;
+
+	if (oldLang !== newLang || !strings) {
+		oldLang = newLang;
+		strings = await getStrings();
+	}
+
+	if (
+		pathname !== "/" &&
+		document.querySelector<HTMLInputElement>("input")?.value
+	) {
+		presenceData.details = `${strings.search} ${
+			document.querySelector<HTMLInputElement>("input")?.value
+		}`;
+		presenceData.smallImageKey = Assets.Search;
+		presence.setActivity(presenceData);
+		return;
+	}
+
+	if (pathname.startsWith("/homepage")) presenceData.details = strings.viewHome;
+	else if (pathname.startsWith("/watch")) {
+		presenceData.smallImageKey = paused ? Assets.Pause : Assets.Play;
+		presenceData.smallImageText = paused ? strings.paused : strings.play;
+		presenceData.largeImageKey = cover
+			? document.querySelector<HTMLImageElement>(".anime-featured > img")
+					?.src ?? Assets.Logo
+			: Assets.Logo;
+		presenceData.details = `${strings.play} ${
+			document.querySelector(".anime-data > h4 > a").textContent
+		}`;
+		presenceData.state = `Episode: ${document
+			.querySelector(".current-episode > .episode-list-item-number")
+			.textContent.replace(/\s/g, "")}`;
+		if (!isNaN(duration) && !paused) {
+			[presenceData.startTimestamp, presenceData.endTimestamp] =
+				presence.getTimestamps(current, duration);
+		}
+	}
 
 	presence.setActivity(presenceData);
 });
